a7466d99947538bac4652a9868ec01fd36db2185
Dovanny <adminsublime@gmail.com>
2026-07-21 03:58:27 -0400

    fix: escuchar en 0.0.0.0 y puerto dinámico para Render

--- pagina-web-sublime/app.py
+++ pagina-web-sublime/app.py
@@ -3431,4 +3431,5 @@ def mis_pedidos():
 
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
